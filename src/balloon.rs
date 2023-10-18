4250dc0514cdb7df7b3572603eccade8a2f48c75
Jax-Hax <jaxhacks543@gmail.com>
2023-10-18 17:08:49 -0400

    added balloon movement function

--- src/balloon.rs
+++ src/balloon.rs
@@ -1,7 +1,22 @@
+use bevy_ecs::{system::{Query, Res, ResMut}, component::Component};
+use vertix::{structs::Instance, state::UpdateInstance};
+
+use crate::map::Map;
+#[derive(Component)]
 pub struct Balloon {
     health: isize,
     bloon_type: usize,
     speed: f32,
     way_point_index: usize,
     damage_dealt: isize, //allows for bloons that gain health?
+}
+fn balloon_movement(mut balloons: Query<(&mut Instance, &mut Balloon)>, map: Res<Map>, mut instance_update: ResMut<UpdateInstance>) {
+    let mut instances = vec![];
+    let mut temp_instance = Instance {..Default::default()};
+    balloons
+        .par_iter_mut()
+        .for_each_mut(|(mut instance, mut balloon)| {
+            
+        });
+    temp_instance.update(instances, &mut instance_update);
 }
--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,7 @@
+use bevy_ecs::system::Resource;
 use glam::Vec2;
-
+#[derive(Resource)]
 pub struct Map {
     spawn_location: Vec2,
+    map_waypoints: Vec<Vec2>,
 }
