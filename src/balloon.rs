37aa114c82bec946c7d1b55b6c6023eb90aa383d
Jax-Hax <jaxhacks543@gmail.com>
2023-10-19 18:05:26 -0400

    added a move towards function now i need to test it

--- src/balloon.rs
+++ src/balloon.rs
@@ -1,6 +1,6 @@
-use bevy_ecs::{system::{Query, Res, ResMut}, component::Component, world::Mut};
-use glam::Vec2;
-use vertix::{structs::Instance, state::UpdateInstance};
+use bevy_ecs::{system::{Query, Res, ResMut, Resource}, component::Component};
+use glam::{Vec2, Vec3};
+use vertix::{prelude::{Instance, delta_time_to_seconds}, resources::{UpdateInstance, DeltaTime}};
 
 use crate::map::Map;
 #[derive(Component)]
@@ -9,18 +9,13 @@ pub struct Balloon {
     bloon_type: usize,
     way_point_index: usize,
 }
-fn move_balloons(mut balloons: Query<(&mut Instance, &mut Balloon)>, map: Res<Map>, mut instance_update: ResMut<UpdateInstance>) {
+fn balloon_movement(mut balloons: Query<(&mut Instance, &mut Balloon)>, map: Res<Map>, mut instance_update: ResMut<UpdateInstance>, balloon_types: Res<BalloonTypes>, delta_time: Res<DeltaTime>) {
     let mut instances = vec![];
     let mut temp_instance = Instance {..Default::default()};
-    balloons
-        .par_iter_mut()
-        .for_each_mut(|(mut instance, mut balloon)| {
-            balloon_movement(instance,balloon,map)
-        });
+    for (mut instance,mut balloon) in &mut balloons {
+        instance.position = move_towards(instance.pos_2d(), map.map_waypoints[balloon.way_point_index], balloon_types.types[balloon.bloon_type].speed * delta_time_to_seconds(delta_time.dt))
+    }
     temp_instance.update(instances, &mut instance_update);
-}
-fn balloon_movement(instance: Mut<'_, Instance>, balloon: Mut<'_, Balloon>, map: Res<'_, Map>) {
-    balloon.
 }
 pub struct BalloonType {
     max_health: usize,
@@ -28,8 +23,12 @@ pub struct BalloonType {
     damage_dealt: isize, //allows for bloons that gain health?
     children: Vec<Box<BalloonType>>,
 }
-fn move_towards(p1: Vec2, p2: Vec2, smooth_val: f32) {
+#[derive(Resource)]
+pub struct BalloonTypes{
+    types: Vec<BalloonType>
+}
+fn move_towards(p1: Vec2, p2: Vec2, smooth_val: f32) -> Vec3 {
     let x = p1.x + p2.x * smooth_val;
     let y = p2.y + (p2.y - p1.y) * (x - p2.x)/(p2.x - p1.x);
-    Vec2::new(x, y);
+    Vec3::new(x, y, 0.)
 }
--- src/map.rs
+++ src/map.rs
@@ -2,6 +2,6 @@ use bevy_ecs::system::Resource;
 use glam::Vec2;
 #[derive(Resource)]
 pub struct Map {
-    spawn_location: Vec2,
-    map_waypoints: Vec<Vec2>,
+    pub spawn_location: Vec2,
+    pub map_waypoints: Vec<Vec2>,
 }
